fe48fd645cbd7a6b2e55509b4354910283adfe9c
L0wry <matthewlowry@hotmail.co.uk>
2018-05-31 17:42:39 +0100

    feat: browser width / height

--- src/bin/snapshotter.js
+++ src/bin/snapshotter.js
@@ -1,16 +1,22 @@
 import webdriver from 'selenium-webdriver';
 import fs from 'fs';
+import logger from '../logger';
 
 export default class SnapShotter {
-  constructor(gridUrl) {
+  constructor({ gridUrl, width = 700, height = 1024 }) {
     this.driver = new webdriver.Builder()
       .usingServer(gridUrl)
       .withCapabilities(webdriver.Capabilities.chrome())
       .build();
+
+    this.driver
+      .manage()
+      .window()
+      .setRect({ width, height });
   }
 
   async takeSnap(url, label) {
-    console.log(`URL and Label: ${url}, ${label}`);
+    logger.info(`Scenario: ${label}`, `Url: ${url}`);
     await this.driver.get(url);
     const screenShot = await this.driver.takeScreenshot();
     fs.writeFileSync(`./newsnaps/${label}.png`, screenShot, 'base64');
--- src/get-screenshots.js
+++ src/get-screenshots.js
@@ -1,9 +1,9 @@
 export default (SnapShotter, config) =>
   Promise.all(
     config.scenarios.map(scenario => {
-      const snap = new SnapShotter(
-        'http://selenium-grid.tnl-dev.ntch.co.uk:4444/wd/hub'
-      );
+      const snap = new SnapShotter({
+        gridUrl: 'http://selenium-grid.tnl-dev.ntch.co.uk:4444/wd/hub'
+      });
 
       const promises = [];
       promises.push(snap.takeSnap(scenario.url, scenario.label));
