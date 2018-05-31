4e625a6ab545f2faf575e07a626ff3599f1a4fb7
L0wry <matthewlowry@hotmail.co.uk>
2018-05-31 18:24:42 +0100

    feat: e2eTests folder

--- src/bin/run.js
+++ src/bin/run.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import path from 'path';
 import logger, { setupLogger } from '../logger';
 import SnapShotter from './snapshotter';
 import getScreenshots from '../get-screenshots';
@@ -14,25 +15,13 @@ program
     '-b, --browser [browser]',
     'Select the browser to run your tests on. E.G. chrome, firefox, etc.'
   )
+  .option('c, --config [config]', 'Path to your config')
   .action(async options => {
-    const config = {
-      gridUrl: 'http://selenium-grid.tnl-dev.ntch.co.uk:4444/wd/hub',
-      scenarios: [
-        {
-          url: 'http://cps-render-ci.elb.tnl-dev.ntch.co.uk/',
-          label: 'homepage',
-          height: 2000,
-          width: 500
-        },
-        {
-          url:
-            'http://cps-render-ci.elb.tnl-dev.ntch.co.uk/profile/fiona-hamilton',
-          label: 'author-profile'
-        }
-      ]
-    };
+    const config = require(path.resolve(options.config)); // eslint-disable-line import/no-dynamic-require
 
     config.browser = options.browser;
+
+    console.log(config);
     logger.info('run', 'Getting snapshots... 📸 ');
     await getScreenshots(SnapShotter, config);
   });
