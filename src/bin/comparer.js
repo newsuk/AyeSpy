c5f9770a662370fcfd5d65b85077ec7b0d2e880d
kaliabadi <kouros_aliabadi@hotmail.com>
2018-06-01 10:30:55 +0100

    fix: resolving createDiff promise in right place

--- src/bin/comparer.js
+++ src/bin/comparer.js
@@ -45,12 +45,11 @@ const createDiffImage = imageData => {
             reject(error);
           }
           logger.info('comprer', 'Image differences have been generated!');
-          resolve();
+          resolve(imageData);
         }
       );
     });
   }
-  return imageData;
 };
 
 export { compare, createDiffImage };
