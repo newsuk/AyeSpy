93cc35f731868a1b50173eb898f08563c8197001
Sam Hendriksen <sam.hendriksen@ecs.co.uk>
2018-06-20 16:59:27 +0100

    test: e2e snap.

--- e2eTests/snap/snap.test.js
+++ e2eTests/snap/snap.test.js
@@ -1,9 +1,8 @@
 /* globals expect */
 
-import { execSync, spawn, spawnSync } from 'child_process';
 import path from 'path';
 import fs from 'fs';
-import selenium from 'selenium-standalone';
+import { execSync } from 'child_process';
 import config from './snapConfig';
 
 describe('e2e Tests taking snaps', () => {
@@ -19,38 +18,18 @@ describe('e2e Tests taking snaps', () => {
     }
   });
 
-  it('Takes snapshot of web page', async () => {
-    //install selenium-standalone
-    console.log('about to install selenium');
-    const selInstallOutput = await spawnSync('selenium-standalone install');
-    selInstallOutput.stdout.on('data', data => {
-      console.log(`stdout: ${data}`);
-    });
-    console.log('after installing selenium');
+  it('should successfully take a snapshot', async () => {
+    let exitCode = 0;
 
-    //start selenium-standalone on a child/background process
-    // console.log('about to start selenium');
-    // const selStartOutput = await spawnSync(
-    //   'selenium-standalone start'
-    // ).toString();
-    // console.log(selStartOutput);
+    try{
+      execSync('node ./lib/bin/run.js snap --browser chrome --config e2eTests/snap/snapConfig.json');
+    }catch (error) {
+      exitCode = error.status;
+    }
 
-    // await execSync('selenium-standalone install');
-    // spawn('selenium-standalone start');
-    // const stdout = await execSync('selenium-standalone start').toString();
-    // console.log(stdout);
-
-    //run the test
-    // console.log('about to run test');
-    // const stdout = await execSync(
-    //   'node ./lib/bin/run.js snap --browser chrome --config e2eTests/snap/snapConfig.json'
-    // ).toString();
-
-    //pipe stdout to Jest console
-    // console.log(stdout);
-    // const files = fs.readdirSync(dirPath);
-    // expect(files).toEqual(['image-large.png']);
-
-    //shut down selenium
+    expect(exitCode).toEqual(0);
+    const latestDirFiles = fs.readdirSync(dirPath);
+    expect(latestDirFiles).toEqual(['image-large.png']);
   });
+
 });
