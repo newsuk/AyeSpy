148e98ccf9cce300e585917ea1563db43fbe8ca6
Matt Lowry <matthewlowry@hotmail.co.uk>
2018-06-25 13:05:10 +0100

    Test: Local e2e tests (#41)
    * test:e2e test for snap command
    
    * test: e2e snap.
    
    * chore: remove uneeded port mapping
    
    * fix: removing e2econfig
    
    * test: adding other e2eTests
    
    * fix: removing selenium webserver
    
    * test: e2e dockerized
    
    * package json updates
    
    * updating package.json
    
    * updating package.json
    
    * package json updates

---
+++ e2eTests/snap/snap.test.js
@@ -0,0 +1,38 @@
+/* globals expect */
+
+import path from 'path';
+import fs from 'fs';
+import { execSync } from 'child_process';
+import config from './snapConfig';
+
+describe('e2e Tests taking snaps', () => {
+  let dirPath;
+
+  beforeEach(() => {
+    dirPath = path.resolve(config.latest);
+
+    if (fs.existsSync(dirPath)) {
+      const files = fs.readdirSync(dirPath);
+      files.forEach(file => fs.unlinkSync(`${dirPath}/${file}`));
+      fs.rmdirSync(dirPath);
+    }
+  });
+
+  it('should successfully take a snapshot', async () => {
+    let exitCode = 0;
+
+    try {
+      const stdout = execSync(
+        'node ./lib/bin/run.js snap --browser chrome --config e2eTests/snap/snapConfig.json'
+      ).toString();
+      //pipe stdout to Jest console
+      console.log(stdout);
+    } catch (error) {
+      exitCode = error.status;
+    }
+
+    expect(exitCode).toEqual(0);
+    const latestDirFiles = fs.readdirSync(dirPath);
+    expect(latestDirFiles).toEqual(['image-large.png']);
+  });
+});
