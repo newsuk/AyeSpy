8204ee2374d0f02fce3f43df3bd7d24cc7dbdff3
L0wry <matthewlowry@hotmail.co.uk>
2019-01-24 19:59:21 +0000

    test: reporter to 100% coverage

--- src/reporter.test.js
+++ src/reporter.test.js
@@ -1,10 +1,14 @@
-/* globals expect it */
+/* globals expect jest it */
 import Reporter from './reporter';
+import logger from './logger';
+
+jest.mock('./logger');
 
 describe('Test reporter', () => {
   let reporter;
 
   beforeEach(() => {
+    jest.clearAllMocks();
     reporter = new Reporter();
   });
 
@@ -24,5 +28,21 @@ describe('Test reporter', () => {
 
   it('throws without failed scenario name', () => {
     expect(() => reporter.fail()).toThrow();
+  });
+
+  it('reports for each failure', () => {
+    reporter.fail('1');
+    reporter.fail('2');
+    reporter.generateReport();
+
+    expect(logger.info).toBeCalledTimes(4);
+  });
+
+  it('reports if there are no failures', () => {
+    reporter.pass('1');
+    reporter.pass('2');
+    reporter.generateReport();
+
+    expect(logger.info).toBeCalledTimes(1);
   });
 });
