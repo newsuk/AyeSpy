b488011362333d42ace4b7daaa1d4deda76ec58c
L0wry <matthewlowry@hotmail.co.uk>
2019-01-25 11:04:20 +0000

    refactor: remove manual mock

--- src/__mocks__/looks-same.js
+++ src/__mocks__/looks-same.js
@@ -1,12 +1,6 @@
-/* globals jest */
+/* globals */
 
 export default (reference, image, opts, callback) => {
   const equal = reference === image ? true : false;
   callback(null, equal);
 };
-
-const createDiff = jest.fn().mockImplementation((config, callback) => {
-  callback(null);
-});
-
-export { createDiff };
