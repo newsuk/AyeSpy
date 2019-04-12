dee24c0c468869d6b4ddff7cb0765e7e1b0d24b4
Lukas Rejman <lukas.rejman@news.co.uk>
2019-04-12 16:27:29 +0100

    feat: Testing E2E scenarios debug added

--- e2eTests/firefox/hideElements/hideElements.test.js
+++ e2eTests/firefox/hideElements/hideElements.test.js
@@ -1,27 +1,8 @@
 /* globals expect */
 
 import { execSync } from 'child_process';
-import path from 'path';
-import fs from 'fs';
-import config from './hideElementsConfig';
-
-function cleanState(dir) {
-  if (fs.existsSync(dir)) {
-    const files = fs.readdirSync(dir);
-    files.forEach(file => fs.unlinkSync(`${dir}/${file}`));
-    fs.rmdirSync(dir);
-  }
-}
 
 describe('e2e Tests hide elements', () => {
-  let latestMockImagesPath;
-
-  beforeEach(() => {
-    latestMockImagesPath = path.resolve(config.latest);
-
-    cleanState(latestMockImagesPath);
-  });
-
   it('hides elements using zero opacity', () => {
     let exitCode = 0;
 
